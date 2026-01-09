17b6c2c8a8cce522305f148d178aa14ee122d315
Joao Victor <joaovictorcostabarbosac@gmail.com>
2026-01-09 17:52:03 -0300

    Cria model de produto

---
+++ backend/app/models/products.py
@@ -0,0 +1,20 @@
+import uuid
+from decimal import Decimal
+
+from typing_extensions import Optional
+
+
+class Product:
+    def __init__(
+        self,
+        name: str,
+        price: Decimal,
+        description: str,
+        stock_quantity: int,
+        id: Optional[uuid.UUID],
+    ) -> None:
+        self.id = id or uuid.uuid4()
+        self.name = name
+        self.price = (price * 10) / 10
+        self.description = description
+        self.stock_quantity = stock_quantity
