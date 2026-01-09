034e9b72d95b5e5876ff731e12fb3f4f04869f63
Joao Victor <joaovictorcostabarbosac@gmail.com>
2026-01-09 17:52:03 -0300

    Atualiza script de criação e estrutura inicial do banco de dados

--- backend/sql/init.sql
+++ backend/sql/init.sql
@@ -6,3 +6,11 @@ CREATE TABLE users (
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
 );
+
+CREATE TABLE products (
+    id UUID PRIMARY KEY,
+    name TEXT NOT NULL,
+    price NUMERIC(10, 2) NOT NULL CHECK (price > 0),
+    description TEXT NOT NULL,
+    stock_quantity INTEGER NOT NULL CHECK (stock_quantity >= 0)
+)
