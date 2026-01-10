0cf19a40f5b31b73e8c23a80f87aadec447f33e1
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-01-10 12:24:04 -0300

    create the page for allow have an specifc route for login

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginCard from "./components/loginCard";
+
+export default function LoginPage() {
+    return (
+        <div className="bg-[url('/images/heroCardImage.png')] bg-cover bg-center w-full h-screen flex flex-col justify-center items-center">
+           <LoginCard/>
+        </div>
+    );
+}
