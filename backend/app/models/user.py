9debb8af768dbc14b47e88c56fd7aa5b070579fe
Joao Victor <joaovictorcostabarbosac@gmail.com>
2026-01-12 23:15:59 -0300

    Refina modelo de usuário

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,5 +1,13 @@
 import uuid
+from enum import Enum
+
 from typing_extensions import Optional
+
+
+class UserRole(str, Enum):
+    USER = "user"
+    ADMIN = "admin"
+
 
 class User:
     def __init__(
@@ -7,9 +15,11 @@ class User:
         name: str,
         email: str,
         password: str,
+        role: UserRole,
         id: Optional[uuid.UUID] = None,
     ) -> None:
         self.id = id or uuid.uuid4()
         self.name = name
         self.email = email
         self.password = password
+        self.role = role
