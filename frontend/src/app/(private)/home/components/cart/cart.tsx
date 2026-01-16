943e290c7fbea56310a0ba3ecfa32be9186a3e00
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-01-15 23:20:36 -0300

    STYLE: refactor the format of the svg and fix the button variant for the new header

--- frontend/src/app/(private)/home/components/cart/cart.tsx
+++ frontend/src/app/(private)/home/components/cart/cart.tsx
@@ -26,7 +26,11 @@ export default function Cart(){
 
   return (
     <div>
-    <button className="bg-teal-950 mr-0 p-2 rounded-lg cursor-pointer hover:shadow-lg hover:shadow-amber-200 transition-all duration-300" onClick={()=> setOpen(!isOpen)} > <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-cart text-amber-200 hover:text-teal-950" viewBox="0 0 16 16"> <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/> </svg> </button>
+    <button className="border-2 border-transparent bg-teal-950 mr-0 p-2 rounded-lg cursor-pointer  hover:bg-amber-200 hover:text-teal-950 hover:border-2 hover:border-teal-950 hover:shadow transition-all duration-300" onClick={()=> setOpen(!isOpen)} > 
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-cart text-amber-200 hover:text-teal-950" viewBox="0 0 16 16"> 
+        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/> 
+      </svg> 
+    </button>
       
       {isOpen && (
         <aside
