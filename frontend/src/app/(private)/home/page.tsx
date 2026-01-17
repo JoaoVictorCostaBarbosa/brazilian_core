da535bc06c5631c7f67f5ddaecf61d0a095ee057
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-01-17 17:28:29 -0300

    FIX: the pagination

--- frontend/src/app/(private)/home/page.tsx
+++ frontend/src/app/(private)/home/page.tsx
@@ -1,47 +1,49 @@
-"use client"
+"use client";
 import HeroCard from "./components/heroCard";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import getProducts from "../../../../api/getAllProducts";
 import SubHeader from "./components/subHeader";
 import RenderParfum from "./components/renderParfum";
 import Header from "./components/header";
+import { Parfum } from "./components/cart/cart";
 
-interface Parfum {
-    id: string,
-    name: string,
-    price: number
-    description: string,
-    stock_quantity: number,
-    url_img: string
-}
 
 export default function HeroSection() {
-    const [parfum, setParfum] = useState<Parfum[]>([]);
-    const [cartItems, setCartItems] = useState<Parfum[]>([])
-    
+  const [parfum, setParfum] = useState<Parfum[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
-    async function getParfum() {
-        try {
-        const products = await getProducts();
+  const LIMIT = 7;
+  
+  async function getParfum(page: number) {
 
-        setParfum(products);
-        } catch (error) {
-        console.error("Error fetching parfums:", error);
-        }
+    try {
+      const start = (page - 1) * LIMIT;
+      const end = start + LIMIT - 1;
+
+      const products = await getProducts(start, end);
+      setParfum(products);
+    } catch (error) {
+      console.error("Error fetching parfums:", error);
     }
+  }
 
-    useEffect(() => {
-        getParfum();
-    }, []);
+  
 
+  useEffect(() => {
+    getParfum(currentPage);
+  }, [currentPage]);
 
-    return(
-        <div>
-            <Header setParfum={setParfum}/>
-            <HeroCard/>
-            <SubHeader/>
-            <RenderParfum parfum={parfum}/>
-        </div>
-    )
+  return (
+    <div>
+      <Header setParfum={setParfum} />
+      <HeroCard />
+      <SubHeader />
+
+      <RenderParfum
+        parfum={parfum}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+    </div>
+  );
 }
