a8cbb6553c84f83ee0c742ac26ef6690f8bc7bb3
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-01-18 19:06:27 -0300

    FEAT: add the coupon for sidebar

--- frontend/src/app/(private)/home/components/sidebar.tsx
+++ frontend/src/app/(private)/home/components/sidebar.tsx
@@ -39,6 +39,7 @@ export default function Sidebar(){
             <nav className="flex flex-col gap-4 mt-6">
               <Link href={"/user"} className="hover:text-amber-400 cursor-pointer text-amber-200 hover:border-b hover:border-amber-400 transition duration-300">Perfil</Link>
               <Link href={"/userReview"} className="hover:text-amber-400 cursor-pointer text-amber-200 hover:border-b hover:border-amber-400 transition duration-300">Ver minhas avaliações</Link>
+              <Link href={"/coupon"} className="hover:text-amber-400 cursor-pointer text-amber-200 hover:border-b hover:border-amber-400 transition duration-300">Ver cupons</Link>
               <a className="hover:text-amber-400 cursor-pointer text-amber-200 hover:border-b hover:border-amber-400 transition duration-300">Configurações</a>
               <a className="hover:text-amber-400 cursor-pointer text-amber-200 hover:border-b hover:border-amber-400 transition duration-300">Sair</a>
             </nav>
