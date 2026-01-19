3395e556ed3a08ce9dc5c4b3086676f46a487097
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-01-18 21:29:42 -0300

    chore: update the imports

--- frontend/api/cart.ts
+++ frontend/api/cart.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
-import { Parfum } from '@/app/(private)/home/components/cart/cart';
+import { Parfum } from '@/app/(private)/cart/page';
 
 
 export interface CartItem extends Parfum {
